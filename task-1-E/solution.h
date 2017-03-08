2ca17e88d5b6b3d0aa28ca3a11895b6d5dd0b5f8
Ilia Igashov <igashov.i@yandex.ru>
2017-03-08 20:48:08 +0300

    Solution of the task-1-E with some corrections: my own functions pow and log2, correct variables' names, changes in data types, etc.

--- task-1-E/solution.h
+++ task-1-E/solution.h
@@ -10,71 +10,98 @@
 #include <thread>
 #include <array>
 #include <vector>
-#include <cmath>
+
+// Power function for integers.
+size_t pow(size_t base, size_t degree)
+{
+    size_t result = 1;
+    for (size_t i = 0; i < degree; ++i) {
+        result *= base;
+    }
+    return result;
+}
+
+// Binary logarithm for integers.
+// Works like ceil(log2(arg)) from <cmath>:
+// if arg is a precise degree of 2 (variable flag indicates it) then the result will be precise too,
+// else the result will be rounded up.
+size_t log2(size_t arg) {
+    bool flag = false;
+    size_t base = 0;
+    while (arg >> 1 != 0) {
+        if ((arg & 1) != 0) {
+            flag = true;
+        }
+        arg >>= 1;
+        ++base;
+    }
+    return flag ? base + 1 : base;
+}
+
 
 class PetersonMutex {
 public:
-    PetersonMutex() {
-        want[0].store(false);
-        want[1].store(false);
-        victim.store(0);
+    PetersonMutex():
+    victim_(0)
+    {
+        want_[0].store(false);
+        want_[1].store(false);
     }
     
-    void lock(int t) {
-        want[t].store(true);
-        victim.store(t);
-        while (want[1 - t].load() && victim.load() == t) {
+    void lock(int thread_id) {
+        want_[thread_id].store(true);
+        victim_.store(thread_id);
+        while (want_[1 - thread_id].load() && victim_.load() == thread_id) {
             std::this_thread::yield();
         }
     }
     
-    void unlock(int t) {
-        want[t].store(false);
+    void unlock(int thread_id) {
+        want_[thread_id].store(false);
     }
     
 private:
-    std::array<std::atomic<bool>, 2> want;
-    std::atomic<int> victim;
+    std::array<std::atomic<bool>, 2> want_;
+    std::atomic<int> victim_;
 };
 
 class TreeMutex {
 public:
     TreeMutex(size_t n_threads):
-    leafs_num(pow(2, ceil(log2(n_threads)))),
-    height(log2(leafs_num)),
-    peterson_mtx(leafs_num - 1)
+    leafs_num_(pow(2, log2(n_threads))),
+    height_(log2(leafs_num_)),
+    peterson_mtx_(leafs_num_ - 1)
     {
     }
     
     // In order to lock the TreeMutex, current_thread has to reach the root of the tree going through
     // Peterson mutexes - nodes on the path in the tree.
     void lock(size_t current_thread) {
-        size_t node = leafs_num - 1 + current_thread; // Index of the node that contains current thread.
-        for (size_t level = 0; level < height; ++level) {
-            size_t peterson_mtx_num = floor((node - 1) / 2); // Peterson mutex index.
-            int peterson_mtx_th_id = node % 2; // Index of the current thread in the Peterson mutex (0 or 1).
-            peterson_mtx[peterson_mtx_num].lock(peterson_mtx_th_id); // Current thread acquires Peterson mutex in this
-            // level.
-            node = peterson_mtx_num; // Since the thread locks in this level, assume we work with new thread with
-            // id = peterson_mtx_num and it wants to lock in higher level.
+        size_t node = leafs_num_ - 1 + current_thread;
+        for (size_t level = 0; level < height_; ++level) {
+            size_t mtx = (node - 1) / 2;
+            bool peterson_mtx_th_id = node % 2;
+            peterson_mtx_[mtx].lock(peterson_mtx_th_id);
+            node = mtx;
         }
-        locked_thread.store(current_thread);
+        locked_thread_.store(current_thread);
     }
     
-    // We will unlock Peterson mutexes in reversed order. In this case we should rebuild the path from the leaf
+    // We will unlock Peterson mutexes in reversed order (the last mutex we locked is always the first one
+    // in our tree: it is a root). In this case we should rebuild the path from the leaf
     // containing current thread to root. Binary code of current_thread keeps all necessary information.
-    // In every level we will take a digit from binary code and determine next Peterson mtx index and thread's id
+    // In every level we will take a digit from binary code and determine next Peterson mtx index and thread's ID
     // in this mutex.
     void unlock(size_t current_thread) {
-        if (locked_thread.load() == current_thread) {
-            int mtx = 0; // The last mutex we locked is always the first one in our tree (it is a root).
-            for (int level = static_cast<int>(height - 1); level >= 0; --level) {
-                int digit = ((current_thread & (1<<level)) == 0) ? 0 : 1; // Take a digit.
-                int idx = 1 - digit; // Determine thread's index in the current Peterson mutex.
-                peterson_mtx[mtx].unlock(idx); // Unlock this mtx.
-                mtx = mtx * 2 + pow(2, digit); // Determine the index of the next mtx on our path.
+        if (locked_thread_.load() == current_thread) {
+            size_t mtx = 0;
+            for (int level = height_ - 1; level >= 0; --level) {
+                int digit = ((current_thread & (1 << level)) == 0) ? 0 : 1;
+                bool peterson_mtx_th_id = 1 - digit;
+                peterson_mtx_[mtx].unlock(peterson_mtx_th_id);
+                mtx = mtx * 2 + pow(2, digit);
             }
-            locked_thread.store(leafs_num);
+            locked_thread_.store(leafs_num_);
         }
     }
     
@@ -82,9 +109,8 @@ public:
     TreeMutex& operator=(const TreeMutex&) = delete;
     
 private:
-    size_t leafs_num; // Number of leafs in the tree.
-    size_t height; // Height of the tree.
-    std::vector<PetersonMutex> peterson_mtx; // Non-leaf nodes of the tree are Peterson mutexes.
-    std::atomic<size_t> locked_thread;
+    size_t leafs_num_;
+    size_t height_;
+    std::vector<PetersonMutex> peterson_mtx_;
+    std::atomic<size_t> locked_thread_;
 };
-
